8ae408c428b23f91a5e91c8da1f66d2be01021bd
dayoojediran <dayoojediran@yahoo.com>
2020-02-10 15:34:26 +0100

    Revert "Initial commit"
    This reverts commit 89a792115da7bb2bc0aa89f82e5a879558303e87.

--- git_rebase.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-## Overwrite local changes from master branch
-
-git fetch --all
-git reset --hard origin/master
--- publica.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git pull origin master
-git add ./vars/httpd_vars.yml ./tmp/* 
-git commit -m "Tower deploy"
-git push -f -u origin master
